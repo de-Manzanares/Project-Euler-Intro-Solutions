b266d9184be8af0923aa44956aa85c5ae94d6482
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2024-01-28 06:31:09 -0800

    update

--- src/Solution_All.h
+++ src/Solution_All.h
@@ -5,5 +5,11 @@
 #include "Solution_1.h"
 #include "Solution_2.h"
 #include "Solution_3.h"
+#include "Solution_4.h"
+#include "Solution_5.h"
+#include "Solution_6.h"
+#include "Solution_7.h"
+#include "Solution_8.h"
+#include "Solution_9.h"
 
 #endif //PROJECTEULERSOLUTIONS_SOLUTION_ALL_H
