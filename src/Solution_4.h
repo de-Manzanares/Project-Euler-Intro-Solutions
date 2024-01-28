035174bf1fc66dc76ff11dbb32f9a08e1b0cbf2f
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2024-01-28 06:24:07 -0800

    Refactor and optimize Solution_4.h code
    This commit simplifies the nesting in the loop for checking palindromes and optimizing the i*j condition. Also, it corrects the type of provenFalse from int to bool.

--- src/Solution_4.h
+++ src/Solution_4.h
@@ -17,10 +17,9 @@ public:
         for (int i = 999; i>99; i--) {
             if (i%11==0) {
                 for (int j = 999; j>99; j--) {
-                    if (checkPalindrome(i*j)) {
-                        if ((temporaryPalindrome = i*j)>maxPalindrome) {
-                            maxPalindrome = temporaryPalindrome;
-                        }
+                    if (checkPalindrome(i*j)
+                            && (temporaryPalindrome = i*j)>maxPalindrome) {
+                        maxPalindrome = temporaryPalindrome;
                     }
                 }
             }
@@ -34,7 +33,7 @@ bool checkPalindrome(int x)
     std::string str_x = std::to_string(x);
     int front = 0;
     int back = static_cast<int>(str_x.size()-1);
-    int provenFalse = false;
+    bool provenFalse = false;
     int i = 0;
 
     while (!provenFalse && i<(str_x.size()/2+str_x.size()%2)) {
