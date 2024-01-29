5dd2318d85ba7aeca2522bfd0f9e854a0882084d
de-Manzanares <138251011+de-Manzanares@users.noreply.github.com>
2024-01-28 20:13:07 -0800

    142913828922 223554341899 ns 223554341 us 223554 ms 223 s 3 min Intel i7-3630QM CPU @ 2.40GHz

---
+++ src/Solution_10.h
@@ -0,0 +1,38 @@
+#ifndef PROJECTEULERSOLUTIONS_SOLUTION_10_H
+#define PROJECTEULERSOLUTIONS_SOLUTION_10_H
+
+class Solution_10 {
+public:
+    static void print_solution(int ceiling)
+    {
+        std::vector<size_t> primeList = {2};
+        size_t number;
+        size_t sum = 0;
+        bool hasPrimeFactor;
+        bool provenComposite;
+        bool generatePrimes = true;
+
+        for (number = 3; generatePrimes; number++) {
+            provenComposite = false;
+            for (int i = 0; i<primeList.size() && !provenComposite; i++) {
+                hasPrimeFactor = (number%primeList[i]==0);
+                if (hasPrimeFactor) {
+                    provenComposite = true;
+                }
+            }
+            if (!provenComposite) {
+                primeList.push_back(number);
+            }
+            generatePrimes = number<SIZE_MAX && primeList.back()<ceiling;
+        }
+        if (primeList.back()>ceiling) {
+            primeList.pop_back();
+        }
+        for (int i = 0; i<primeList.size(); i++) {
+            sum += primeList[i];
+        }
+        std::cout << sum << std::endl;
+    }
+};
+
+#endif //PROJECTEULERSOLUTIONS_SOLUTION_10_H
