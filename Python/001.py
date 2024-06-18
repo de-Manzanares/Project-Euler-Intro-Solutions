44a13ad0feba240fca5ca690bd79a0566863fd91
de-Manzanares <git.in.touch@dmanz.org>
2024-06-18 13:30:02 -0700

    .

--- Python/001.py
+++ Python/001.py
@@ -3,7 +3,9 @@
 #   The sum of these multiples is 23.
 #   Find the sum of all the multiples of 3 or 5 below 1000.
 
-Sum = 0
+total = 0
+
 for i in range(1, 1000):
-    if i % 3 == 0 or i % 5 == 0: Sum += i
-print(Sum)
+    if i % 3 == 0 or i % 5 == 0:
+        total += i
+print(total)
