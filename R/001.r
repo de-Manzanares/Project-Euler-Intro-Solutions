1cf503e37d4d485d64263b2a1bb21d8a3da61c88
de-Manzanares <git.in.touch@dmanz.org>
2024-06-18 20:57:23 -0700

    .

--- R/001.r
+++ R/001.r
@@ -3,6 +3,6 @@
 #   The sum of these multiples is 23.
 #   Find the sum of all the multiples of 3 or 5 below 1000.
 
-sum <- 0
-for (i in 1:999) { if (i %% 3 == 0 || i %% 5 == 0) { sum <- sum + i } }
-print(sum)
+total <- 0
+for (i in 1:999) { if (i %% 3 == 0 || i %% 5 == 0) { total <- total + i } }
+print(total)
