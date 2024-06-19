103163767d26c8f0f0c3711456db2b208c0ceb42
de-Manzanares <git.in.touch@dmanz.org>
2024-06-19 00:21:33 -0700

    .

---
+++ Julia/003.jl
@@ -0,0 +1,24 @@
+#=
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143?
+=#
+
+function f(n)
+    while iseven(n)         # there is only one even prime :)
+        n /= 2
+    end
+
+    i = 3
+    while n != 1
+        while n % i == 0    # while i evenly divides n
+            n /= i          # n = n/i
+        end
+        i += 2              # move to next odd number (there is only one even prime)
+    end
+
+    i -= 2                  # account for extra increment after exiting the while loop
+
+    return i
+end
+
+println(f(600851475143))
